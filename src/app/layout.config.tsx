5f22b58b10541f8c4cce1c1ca8d27c3c17053859
boyce <w@ojbk.io>
2025-03-23 00:31:38 +0800

    add logo

--- src/app/layout.config.tsx
+++ src/app/layout.config.tsx
@@ -8,23 +8,16 @@ import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
  * Docs Layout: app/docs/layout.tsx
  */
 export const baseOptions: BaseLayoutProps = {
+  disableThemeSwitch: true,
   nav: {
     title: (
       <>
-        <svg
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-label="Logo"
-        >
-          <circle
-            cx={12}
-            cy={12}
-            r={12}
-            fill="currentColor"
-          />
-        </svg>
-        {process.env.SITE_NAME}
+        <img
+          className="w-[24px] h-[24px] rounded-full"
+          src="/quka-avatar.jpg"
+          alt="logo"
+        />
+        {process.env.siteName}
       </>
     ),
   },
